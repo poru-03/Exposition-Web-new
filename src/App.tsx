695003eae79af24365cb6e1f49d254ee3f0606b8
poru-03 <malporudulaj@gmail.com>
2026-09-06 23:59:03 +0530

    Update import for HeroSection component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import ScrollProgressBar from './components/ScrollProgressBar';
 import Navbar from './components/Navbar';
 import AboutSection from './sections/AboutSection';
 import FooterSection from './sections/FooterSection';
-import HeroSection from './sections/CurtainHeroSection';
+import HeroSection from './sections/HeroSection';
 import InterviewHighlightsSection from './sections/InterviewHighlightsSection';
 import KeynoteSpeakersSection from './sections/KeynoteSpeakersSection';
 import PartnersSection from './sections/PartnersSection';
